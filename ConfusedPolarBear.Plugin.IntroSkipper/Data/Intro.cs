cfbe4ab4419367def558ad7c04ed1eeb43a0df7e
Kilian von Pflugk <github@jumoog.io>
2024-09-07 22:24:05 +0200

    cleanup Intros

--- ConfusedPolarBear.Plugin.IntroSkipper/Data/Intro.cs
+++ ConfusedPolarBear.Plugin.IntroSkipper/Data/Intro.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Runtime.Serialization;
-using System.Text.Json.Serialization;
 
 namespace ConfusedPolarBear.Plugin.IntroSkipper.Data;
 
@@ -8,25 +7,18 @@ namespace ConfusedPolarBear.Plugin.IntroSkipper.Data;
 /// Result of fingerprinting and analyzing two episodes in a season.
 /// All times are measured in seconds relative to the beginning of the media file.
 /// </summary>
+/// <remarks>
+/// Initializes a new instance of the <see cref="Intro"/> class.
+/// </remarks>
+/// <param name="intro">intro.</param>
 [DataContract(Namespace = "http://schemas.datacontract.org/2004/07/ConfusedPolarBear.Plugin.IntroSkipper")]
-public class Intro
+public class Intro(Segment intro)
 {
-    /// <summary>
-    /// Initializes a new instance of the <see cref="Intro"/> class.
-    /// </summary>
-    /// <param name="intro">intro.</param>
-    public Intro(Segment intro)
-    {
-        EpisodeId = intro.EpisodeId;
-        IntroStart = intro.Start;
-        IntroEnd = intro.End;
-    }
-
     /// <summary>
     /// Gets or sets the Episode ID.
     /// </summary>
     [DataMember]
-    public Guid EpisodeId { get; set; }
+    public Guid EpisodeId { get; set; } = intro.EpisodeId;
 
     /// <summary>
     /// Gets a value indicating whether this introduction is valid or not.
@@ -35,22 +27,16 @@ public class Intro
     public bool Valid => IntroEnd > 0;
 
     /// <summary>
-    /// Gets the duration of this intro.
-    /// </summary>
-    [JsonIgnore]
-    public double Duration => IntroEnd - IntroStart;
-
-    /// <summary>
     /// Gets or sets the introduction sequence start time.
     /// </summary>
     [DataMember]
-    public double IntroStart { get; set; }
+    public double IntroStart { get; set; } = intro.Start;
 
     /// <summary>
     /// Gets or sets the introduction sequence end time.
     /// </summary>
     [DataMember]
-    public double IntroEnd { get; set; }
+    public double IntroEnd { get; set; } = intro.End;
 
     /// <summary>
     /// Gets or sets the recommended time to display the skip intro prompt.
