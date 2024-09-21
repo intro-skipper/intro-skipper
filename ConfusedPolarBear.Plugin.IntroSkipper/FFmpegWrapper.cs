bc24e7aeed1555f3929a86b9cea612dce381103a
Kilian von Pflugk <github@jumoog.io>
2024-09-21 14:41:14 +0200

    move ShowInfos add logDebug to DeleteEpisodeCache

--- ConfusedPolarBear.Plugin.IntroSkipper/FFmpegWrapper.cs
+++ ConfusedPolarBear.Plugin.IntroSkipper/FFmpegWrapper.cs
@@ -619,6 +619,7 @@ public static partial class FFmpegWrapper
         foreach (var filePath in Directory.EnumerateFiles(Plugin.Instance!.FingerprintCachePath, filePattern))
         {
 #pragma warning disable CA3003 // Comments should contain text
+            Logger?.LogDebug("DeleteEpisodeCache {FilePath}", filePath);
             File.Delete(filePath);
 #pragma warning restore CA3003 // Comments should contain text
         }
