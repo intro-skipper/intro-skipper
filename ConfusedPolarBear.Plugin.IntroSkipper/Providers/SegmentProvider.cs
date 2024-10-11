23ae1fb2ec064e8a070171099b8824a4adf0ba95
rlauu <46294892+rlauu@users.noreply.github.com>
2024-10-11 20:39:01 +0200

    Update SegmentProvider.cs

--- ConfusedPolarBear.Plugin.IntroSkipper/Providers/SegmentProvider.cs
+++ ConfusedPolarBear.Plugin.IntroSkipper/Providers/SegmentProvider.cs
@@ -15,18 +15,14 @@ namespace ConfusedPolarBear.Plugin.IntroSkipper.Providers
     /// <summary>
     /// Introskipper media segment provider.
     /// </summary>
-    public class SegmentProvider : IMediaSegmentProvider
+    /// <remarks>
+    /// Initializes a new instance of the <see cref="SegmentProvider"/> class.
+    /// </remarks>
+    /// <param name="logger">Instance of the <see cref="ILogger{SegmentProvider}"/> interface.</param>
+    public class SegmentProvider(ILogger<SegmentProvider> logger) : IMediaSegmentProvider
     {
-        private readonly ILogger<SegmentProvider> _logger;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="SegmentProvider"/> class.
-        /// </summary>
-        /// <param name="logger">Instance of the <see cref="ILogger{SegmentProvider}"/> interface.</param>
-        public SegmentProvider(ILogger<SegmentProvider> logger)
-        {
-            _logger = logger;
-        }
+        private readonly ILogger<SegmentProvider> _logger = logger;
+        private int _remainingSecondsOfIntro = Plugin.Instance!.Configuration.RemainingSecondsOfIntro;
 
         /// <inheritdoc/>
         public string Name => Plugin.Instance!.Name;
@@ -34,17 +30,14 @@ namespace ConfusedPolarBear.Plugin.IntroSkipper.Providers
         /// <inheritdoc/>
         public Task<IReadOnlyList<MediaSegmentDto>> GetMediaSegments(MediaSegmentGenerationRequest request, CancellationToken cancellationToken)
         {
-            _logger.LogInformation("Getting media segments for item {ItemId}", request.ItemId);
-
             var segments = new List<MediaSegmentDto>();
 
             if (Plugin.Instance!.Intros.TryGetValue(request.ItemId, out var introValue))
             {
-                _logger.LogDebug("Found intro for item {ItemId}", request.ItemId);
                 segments.Add(new MediaSegmentDto
                 {
                     StartTicks = TimeSpan.FromSeconds(introValue.Start).Ticks,
-                    EndTicks = TimeSpan.FromSeconds(introValue.End).Ticks,
+                    EndTicks = TimeSpan.FromSeconds(introValue.End - _remainingSecondsOfIntro).Ticks,
                     ItemId = request.ItemId,
                     Type = MediaSegmentType.Intro
                 });
@@ -52,26 +45,23 @@ namespace ConfusedPolarBear.Plugin.IntroSkipper.Providers
 
             if (Plugin.Instance!.Credits.TryGetValue(request.ItemId, out var creditValue))
             {
-                _logger.LogDebug("Found outro for item {ItemId}", request.ItemId);
                 segments.Add(new MediaSegmentDto
                 {
                     StartTicks = TimeSpan.FromSeconds(creditValue.Start).Ticks,
-                    EndTicks = TimeSpan.FromSeconds(creditValue.End).Ticks,
+                    EndTicks = TimeSpan.FromSeconds(creditValue.End - _remainingSecondsOfIntro).Ticks,
                     ItemId = request.ItemId,
                     Type = MediaSegmentType.Outro
                 });
             }
 
-            _logger.LogInformation("Found {SegmentCount} segments for item {ItemId}", segments.Count, request.ItemId);
+            _logger.LogDebug("Found {SegmentCount} segments for item {ItemId}", segments.Count, request.ItemId);
             return Task.FromResult<IReadOnlyList<MediaSegmentDto>>(segments);
         }
 
         /// <inheritdoc/>
         public ValueTask<bool> Supports(BaseItem item)
         {
-            var isSupported = item is Episode;
-            _logger.LogInformation("Support check for item {ItemId}: {IsSupported}", item.Id, isSupported);
-            return ValueTask.FromResult(isSupported);
+            return ValueTask.FromResult(item is Episode);
         }
     }
 }
