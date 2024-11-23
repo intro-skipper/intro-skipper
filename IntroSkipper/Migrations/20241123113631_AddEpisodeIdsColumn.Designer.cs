af74ec1e6b58543d6b3d7a774e1422845b4d2095
Kilian von Pflugk <github@jumoog.io>
2024-11-23 12:50:53 +0100

    add missing Migration for EpisodeIds

--- IntroSkipper/Migrations/20241123113631_AddEpisodeIdsColumn.Designer.cs
+++ IntroSkipper/Migrations/20241123113631_AddEpisodeIdsColumn.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using IntroSkipper.Db;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IntroSkipper.Migrations
 {
     [DbContext(typeof(IntroSkipperDbContext))]
-    partial class IntroSkipperDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241123113631_AddEpisodeIdsColumn")]
+    partial class AddEpisodeIdsColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.11");
---
+++ IntroSkipper/Migrations/20241123113631_AddEpisodeIdsColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IntroSkipper.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEpisodeIdsColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EpisodeIds",
+                table: "DbSeasonInfo",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: string.Empty);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EpisodeIds",
+                table: "DbSeasonInfo");
+        }
+    }
+}
