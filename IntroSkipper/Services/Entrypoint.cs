70ca2d90584aa101d1ece290eb858e8a9078dfa7
TwistedUmbrellaX <1173913+AbandonedCart@users.noreply.github.com>
2025-01-17 09:45:42 -0500

    Update Entrypoint.cs

--- IntroSkipper/Services/Entrypoint.cs
+++ IntroSkipper/Services/Entrypoint.cs
@@ -143,7 +143,9 @@ namespace IntroSkipper.Services
         /// <param name="eventArgs">The <see cref="TaskCompletionEventArgs"/>.</param>
         private void OnLibraryRefresh(object? sender, TaskCompletionEventArgs eventArgs)
         {
+            // We only want to trigger this when nothing was added or changed
             if (_config.AutoDetectIntros &&
+                _analyzeAgain != true
                 eventArgs.Result is { Key: "RefreshLibrary", Status: TaskCompletionStatus.Completed } &&
                 AutomaticTaskState != TaskState.Running)
             {
