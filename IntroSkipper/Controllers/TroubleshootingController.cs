ba09da5f45ff1229035693463e0d443d5ea75b50
Kilian von Pflugk <github@jumoog.io>
2025-06-13 20:47:21 +0200

    Revert "remove storage helper as its now visible in the dashboard (#535)"
    This reverts commit 71c04df35a4bfb7a4d6c6b62cc694098a762573b.
    
    The dashboard does not show an overview of library usage.

--- IntroSkipper/Controllers/TroubleshootingController.cs
+++ IntroSkipper/Controllers/TroubleshootingController.cs
@@ -113,4 +113,61 @@ public class TroubleshootingController : ControllerBase
 
         return bundle.ToString();
     }
+
+    /// <summary>
+    /// Gets a Markdown formatted support bundle.
+    /// </summary>
+    /// <response code="200">Support bundle created.</response>
+    /// <returns>Support bundle.</returns>
+    [HttpGet("Storage")]
+    [Produces(MediaTypeNames.Text.Plain)]
+    public ActionResult<string> GetFreeSpace()
+    {
+        ArgumentNullException.ThrowIfNull(Plugin.Instance);
+        var bundle = new StringBuilder();
+
+        var libraries = _libraryManager.GetVirtualFolders();
+        foreach (var library in libraries)
+        {
+            try
+            {
+                DriveInfo driveInfo = new DriveInfo(library.Locations[0]);
+                // Get available free space in bytes
+                long availableFreeSpace = driveInfo.AvailableFreeSpace;
+
+                // Get total size of the drive in bytes
+                long totalSize = driveInfo.TotalSize;
+
+                // Get total used space in Percentage
+                double usedSpacePercentage = totalSize > 0 ? (totalSize - availableFreeSpace) / (double)totalSize * 100 : 0;
+
+                bundle.Append(CultureInfo.CurrentCulture, $"Library: {library.Name}\n");
+                bundle.Append(CultureInfo.CurrentCulture, $"Drive: {driveInfo.Name}\n");
+                bundle.Append(CultureInfo.CurrentCulture, $"Total Size: {GetHumanReadableSize(totalSize)}\n");
+                bundle.Append(CultureInfo.CurrentCulture, $"Available Free Space: {GetHumanReadableSize(availableFreeSpace)}\n");
+                bundle.Append(CultureInfo.CurrentCulture, $"Total used in Percentage: {Math.Round(usedSpacePercentage, 2)}%\n\n");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogWarning("Unable to get DriveInfo: {Exception}", ex);
+            }
+        }
+
+        return bundle.ToString().TrimEnd('\n');
+    }
+
+    private static string GetHumanReadableSize(long bytes)
+    {
+        string[] sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+        double len = bytes;
+        int order = 0;
+
+        while (len >= 1024 && order < sizes.Length - 1)
+        {
+            order++;
+            len /= 1024;
+        }
+
+        return $"{len:0.##} {sizes[order]}";
+    }
 }
