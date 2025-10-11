035902cb7cbbe145a1ae23579ef89d1e18e59138
Kilian von Pflugk <github@jumoog.io>
2025-10-11 11:47:42 +0200

    migrate repo url from manifest.intro-skipper.org -> intro-skipper.org

--- IntroSkipper/Helper/LegacyMigrations.cs
+++ IntroSkipper/Helper/LegacyMigrations.cs
@@ -14,6 +14,7 @@ using MediaBrowser.Controller.Library;
 using MediaBrowser.Model.Entities;
 using MediaBrowser.Model.Updates;
 using Microsoft.Extensions.Logging;
+using static System.Net.WebRequestMethods;
 
 namespace IntroSkipper.Helper;
 
@@ -80,7 +81,8 @@ internal static class LegacyMigrations
             [
                 "https://raw.githubusercontent.com/intro-skipper/intro-skipper/master/manifest.json",
                 "https://raw.githubusercontent.com/jumoog/intro-skipper/master/manifest.json",
-                "https://manifest.intro-skipper.workers.dev/manifest.json"
+                "https://manifest.intro-skipper.workers.dev/manifest.json",
+                "https://manifest.intro-skipper.org/manifest.json"
             ];
 
             var config = serverConfiguration.Configuration;
@@ -90,12 +92,12 @@ internal static class LegacyMigrations
             {
                 pluginRepositories.RemoveAll(repo => repo.Url != null && oldRepos.Contains(repo.Url));
 
-                if (!pluginRepositories.Exists(repo => repo.Url == "https://manifest.intro-skipper.org/manifest.json") && plugin.Configuration.OverrideManifestUrl)
+                if (!pluginRepositories.Exists(repo => repo.Url == "https://intro-skipper.org/manifest.json") && plugin.Configuration.OverrideManifestUrl)
                 {
                     pluginRepositories.Add(new RepositoryInfo
                     {
-                        Name = "intro skipper (automatically migrated by plugin)",
-                        Url = "https://manifest.intro-skipper.org/manifest.json",
+                        Name = "intro skipper Plugin Repository (automatically migrated by plugin)",
+                        Url = "https://intro-skipper.org/manifest.json",
                         Enabled = true,
                     });
                 }
