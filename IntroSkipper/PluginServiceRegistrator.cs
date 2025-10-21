ef5365e90a6f8f1630ae433b8ddfde8d227304e4
Kilian von Pflugk <github@jumoog.io>
2025-10-21 12:15:24 +0200

    not needed

--- IntroSkipper/PluginServiceRegistrator.cs
+++ IntroSkipper/PluginServiceRegistrator.cs
@@ -19,7 +19,6 @@ namespace IntroSkipper
         /// <inheritdoc />
         public void RegisterServices(IServiceCollection serviceCollection, IServerApplicationHost applicationHost)
         {
-            serviceCollection.AddSingleton<QueueManager>();
             serviceCollection.AddHostedService<AutoSkip>();
             serviceCollection.AddHostedService<Entrypoint>();
             serviceCollection.AddSingleton<IMediaSegmentProvider, SegmentProvider>();
