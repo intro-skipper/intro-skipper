10514c716c06e6976cd26ffbb1c7166af5e04145
rlauuzo <46294892+rlauuzo@users.noreply.github.com>
2026-05-02 16:16:32 +0200

    Create AssemblyInfo.cs

---
+++ IntroSkipper/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+// SPDX-FileCopyrightText: 2026 Intro-Skipper contributors <intro-skipper.org>
+// SPDX-License-Identifier: GPL-3.0-only
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("IntroSkipper.Tests")]
