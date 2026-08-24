53222c97cd90bf91a1fa5ccbcc4a468c09ff0fc2
TwistedUmbrellaX <twistedumbrella@gmail.com>
2026-08-23 22:47:08 -0400

    Add LINQ import to TestQueueManager

--- IntroSkipper.Tests/TestQueueManager.cs
+++ IntroSkipper.Tests/TestQueueManager.cs
@@ -6,6 +6,7 @@ namespace IntroSkipper.Tests;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using IntroSkipper.Configuration;
