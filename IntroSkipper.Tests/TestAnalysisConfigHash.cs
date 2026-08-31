7cfc75eb24591c6d335ae4657d4d90525062b863
TwistedUmbrellaX <twistedumbrella@gmail.com>
2026-08-31 10:01:15 -0400

    Add System namespace to analysis config hash tests

--- IntroSkipper.Tests/TestAnalysisConfigHash.cs
+++ IntroSkipper.Tests/TestAnalysisConfigHash.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2026 rlauuzo
 // SPDX-License-Identifier: GPL-3.0-only
 
+using System;
 using IntroSkipper.Configuration;
 using IntroSkipper.Data;
 using IntroSkipper.Helper;
