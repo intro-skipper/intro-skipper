dec177c68492b821de8067a19735c70bc9e790db
TwistedUmbrellaX <twistedumbrella@gmail.com>
2026-09-09 15:09:58 -0400

    Remove stray BOMs from migration files

--- IntroSkipper/Migrations/20260822154223_InitialCreate.cs
+++ IntroSkipper/Migrations/20260822154223_InitialCreate.cs
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2026 rlauuzo
 // SPDX-FileCopyrightText: 2026 AbandonedCart
 // SPDX-License-Identifier: GPL-3.0-only
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
--- IntroSkipper/Migrations/20260830155030_AddSegmentProjectionJournal.cs
+++ IntroSkipper/Migrations/20260830155030_AddSegmentProjectionJournal.cs
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2026 rlauuzo
 // SPDX-License-Identifier: GPL-3.0-only
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
